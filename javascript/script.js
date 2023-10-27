2ad2d2b3802eab09a41159589222af9cb56c5517
Janis Schiller <schillerjanis@gmail.com>
2023-10-27 10:14:53 +0200

    changes

---
+++ javascript/script.js
@@ -0,0 +1,46 @@
+
+
+
+// function declaration
+function printInvitation() {
+    console.log(`Dear ${name}`)
+    console.log('Iwould love to see you at my wedding.')
+    console.log('Hope you can make it')
+}
+
+// function execution / function call
+printInvitation('Rusmir'); 
+printInvitation('Amgad'); 
+
+
+/* //  NICHT SO MACHEN 
+let number1 = 3;
+let number2 = 4;
+
+function addTwoNumbers() {
+    console.log(number1 + number2)
+}
+
+addTwoNumbers(); */
+
+
+
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2)
+}
+
+addTwoNumbers(3,4);
+
+
+
+
+function addThreeNumbers(number1, number2, number3) {
+    const sum = number1 + number2 + number3;
+
+    console.log(sum);
+}
+
+addThreeNumbers(3,4,5);
+
+
